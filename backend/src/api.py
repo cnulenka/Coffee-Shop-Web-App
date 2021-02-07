9c33f8e8683a8af03ceb055d671dae1d97732f90
cnulenka <sinulenka001@gmail.com>
2021-02-08 02:05:48 +0530

    error handling decorators

--- backend/src/api.py
+++ backend/src/api.py
@@ -91,3 +91,41 @@ def delete_drinks(jwtoken, drink_id):
     except:
         abort(422)
 
+## Error Handling
+
+@app.errorhandler(422)
+def unprocessable(error):
+    return jsonify({
+                    "success": False, 
+                    "error": 422,
+                    "message": "unprocessable"
+                    }), 422
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"success": False,
+                    "error": 404,
+                    "message": "resource not found"
+                    }),404
+
+@app.errorhandler(500)
+def server_error(error):
+    return jsonify({"success": False,
+                    "error": 500,
+                    "message": "server error"
+                    }), 500
+
+@app.errorhandler(401)
+def unauthorized(error):
+    return jsonify({"success": False,
+                    "error": 401,
+                    "message": "unauthorized"
+                    }), 401
+
+@app.errorhandler(AuthError)
+def authorization_error(error):
+    return jsonify({
+        "success": False,
+        "error": error.status_code,
+        "message": error.error['description']
+    }), error.status_code
